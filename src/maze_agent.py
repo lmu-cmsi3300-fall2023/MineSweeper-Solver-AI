75dc4a1eab84a1e49ae27cb3f8b1b75d687dc168
Matthew Lee <mlee164@lion.lmu.edu>
2023-10-03 19:30:58 -0700

    distance?

--- src/maze_agent.py
+++ src/maze_agent.py
@@ -57,7 +57,7 @@ class MazeAgent:
         self.safe_tiles.add(self.goal)
         
         #add initial location to safetiles
-        self.kb.tell(MazeClause([((Constants.PIT_BLOCK, self.env._initial_loc),False)]))
+        self.kb.tell(MazeClause([((Constants.PIT_BLOCK, self.startLoc),False)]))
         self.safe_tiles.add(self.env._initial_loc)
         
         #goal can not have 4 pits around it
@@ -65,7 +65,7 @@ class MazeAgent:
         self.kb.tell(MazeClause([(("P", tile), False) for tile in self.env.get_cardinal_locs(self.goal, 1)]))
 
         #Use this to keep track of the agent's current location
-        self.think(perception)      
+        self.think(perception)
         
     ##################################################################
     # Methods
@@ -114,7 +114,6 @@ class MazeAgent:
 
         pit_locations = self.env.get_cardinal_locs(loc, 1) - self.safe_tiles
         comb = combinations(pit_locations, 2)
-        
 
         match tileType:
 
@@ -162,7 +161,7 @@ class MazeAgent:
                 self.kb.tell(MazeClause([(("P", loc), True)]))
             
         self.kb.simplify_from_known_locs(self.kb.clauses, self.safe_tiles, self.pit_tiles)
-
+        self.kb.simplify_self(self.pit_tiles, self.safe_tiles)
         #part 3
         #Check if any possible pits are now definitely safe or not
         self.scanKB(loc)
@@ -175,50 +174,46 @@ class MazeAgent:
         # Initialize priority outside the loop
         best_tile = (0,0)
         best_distance = 100
-        weight:float = 1
-        
-        
+        weight:float = 1   
 
         for tile in frontier:
             # Update priority based on the current tileType
             if tile in self.pit_tiles:
                 break
-            
+
+            distance = 0
+
+            # if float(abs(tile[0] - loc[0]) - abs(tile[1] - loc[1])) > 1:
+            #     distance += (abs(tile[0] - loc[0]) + abs(tile[1] - loc[1]))
+
             match tileType:
                 case ".":
-                    weight = 1.25
+                    weight = 1.25 + distance
                 case "3":
-                    weight = 1.5
+                    weight = 1.5 + distance
                 case "2":
-                    weight = 1.5
+                    weight = 1.5 + distance
                 case "1":
-                    weight = 1
+                    weight = 1 + distance
                 case "P":
-                    weight = 1
-                    
+                    weight = 1 + distance
+
+            # Manhattan Distance for priority
             if tile in self.safe_tiles or tile not in self.pit_tiles:
                 mDist = int(abs(tile[0] - self.goal[0]) + abs(tile[1] - self.goal[1]*(weight)))
-                print(mDist)
+                # print(mDist)
                 if mDist < best_distance:
                     best_distance = mDist
                     best_tile = tile
-                 
-            
-            
-                    
+
+        # Sort based on priority and Manhattan Distance
         if best_tile == (0,0):
             best_tile = min(
                 frontier - self.pit_tiles,
                 key = lambda tile: abs(tile[0] - self.goal[0]) + abs(tile[1] - self.goal[1]*weight)
             )
 
-            # Manhattan Distance for priority
-
-        # Sort based on priority and Manhattan Distance
         return best_tile
-        
-
-        # return random.choice(list(frontier))
         
     def is_safe_tile (self, loc: tuple[int, int ]) -> Optional[bool]:
         """
